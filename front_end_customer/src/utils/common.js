84f574cde082f0646beb6d570a4ca508c1358ee9
Esteban Eduardo Garcia Ojeda <esteban.e.garcia.o@gmail.com>
2022-02-24 15:51:56 -0600

    solved

--- front_end_customer/src/utils/common.js
+++ front_end_customer/src/utils/common.js
@@ -1,5 +1,5 @@
 const Common = {
-    url_api: "http://127.0.0.1:5000/api/customers",
+    url_api: "https://customersapi.pythonanywhere.com/api/customers",
     id: "ID",
     full_name: "Nombre completo",
     phone: "Telefono",
