661253a4c99e2db4b85d455770f9ac8dc2d100ad
Esteban Eduardo Garcia Ojeda <esteban.e.garcia.o@gmail.com>
2022-02-24 15:31:24 -0600

    solved

--- customers/utils/db.py
+++ customers/utils/db.py
@@ -3,9 +3,9 @@ from flask_marshmallow import Marshmallow
 db = SQLAlchemy()
 ma = Marshmallow()
 
-user_db = 'root'
+user_db = 'customersApi'
 password_db = '$1A2b3c4d'
-server_db = 'localhost'
-name_db = 'custumers_api'
+server_db = 'customersApi.mysql.pythonanywhere-services.com'
+name_db = 'customersApi$customers_api'
 
 uri_db = 'mysql://' + user_db + ':' + password_db + '@' + server_db + '/' + name_db
